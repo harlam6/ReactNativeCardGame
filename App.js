6c7f46188e578601b7de54ffa10942848a98f1e9
Lars Lam <theveryasian@gmail.com>
2019-05-18 09:34:55 -0700

    test touchable opacity and touch image

--- App.js
+++ App.js
@@ -7,9 +7,7 @@
  */
 
 import React, {Component} from 'react';
-import { Platform, StyleSheet, Button, Image, Text, View, TouchableHighlight } from 'react-native';
-
-
+import { Alert, Platform, StyleSheet, Button, Image, Text, View, TouchableHighlight, TouchableOpacity } from 'react-native';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -20,9 +18,17 @@ const instructions = Platform.select({
 
 type Props = {};
 export default class App extends Component<Props> {
+
+    callFun = () => {
+        alert("Image Clicked!!!");
+    }
+
     render() {
+
+
         let pic = {
             uri: 'https://upload.wikimedia.org/wikipedia/commons/d/de/Bananavarieties.jpg'
+
         };
 
 
@@ -35,12 +41,33 @@ export default class App extends Component<Props> {
                     Alert.alert('You tapped the button!');
                 }}
                 title="Press Me"
-            />
-            <Image source={pic} style={{ width: 193, height: 110 }} />
+                />
+
+              
+           
+
+
+                <Image source={pic} style={{ width: 193, height: 110 }} />
+                <Image onPress={() => {
+                    Alert.alert('You tapped the button!');
+                }} source={require('./Samurai.png')}
+
+                    style={{ width: 193, height: 110 }} />
+        
+        
         <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
                 <Text style={styles.instructions}>{instructions}</Text>
 
+
+                <TouchableOpacity activeOpacity={.5} onPress={this.callFun}>
+
+                    <Image source={require('./Samurai.png')} style={{ width: 193, height: 110 }} />
+
+                </TouchableOpacity>
+
+
+               
       </View>
     );
   }
